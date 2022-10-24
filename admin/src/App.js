b125a5218910a818f60e2234b72f3c8774282601
xuanson <user@xuanson-MacBook-Pro.local>
2022-10-25 06:17:50 +0900

    update 10.25

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,47 +1,75 @@
-import Sidebar from "./components/sidebar/Sidebar";
-import Topbar from "./components/topbar/Topbar";
-import "./App.css";
-import Home from "./pages/home/Home";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import UserList from "./pages/userList/UserList";
-import User from "./pages/user/User";
-import NewUser from "./pages/newUser/NewUser";
-import ProductList from "./pages/productList/ProductList";
-import Product from "./pages/product/Product";
-import NewProduct from "./pages/newProduct/NewProduct";
+import Sidebar from './components/sidebar/Sidebar';
+import Topbar from './components/topbar/Topbar';
+import './app.css';
+import Home from './pages/home/Home';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import UserList from './pages/userList/UserList';
+import User from './pages/user/User';
+import NewUser from './pages/newUser/NewUser';
+import ProductList from './pages/productList/ProductList';
+import Product from './pages/product/Product';
+import NewProduct from './pages/newProduct/NewProduct';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import axios from 'axios';
+import { useMemo } from 'react';
 
 function App() {
-  return (
-    <Router>
-      <Topbar />
-      <div className="container">
-        <Sidebar />
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/users">
-            <UserList />
-          </Route>
-          <Route path="/user/:userId">
-            <User />
-          </Route>
-          <Route path="/newUser">
-            <NewUser />
-          </Route>
-          <Route path="/products">
-            <ProductList />
-          </Route>
-          <Route path="/product/:productId">
-            <Product />
-          </Route>
-          <Route path="/newproduct">
-            <NewProduct />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
-  );
+    const MONTHS = useMemo(() => ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']);
+
+    const [userStats, setUserStats] = useState([]);
+
+    useEffect(() => {
+        const getStats = async () => {
+            try {
+                const res = await axios.get('/users/stats', {
+                    headers: {
+                        token: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzNGRiYWIxODA4NjQ4NjI2ODI1MWY2MCIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY2NjM5OTM5MSwiZXhwIjoxNjY2ODMxMzkxfQ.xjx_SgXKKsqXshkbmmlEg4wN3ae2Et4P9-aUSJfEHKk',
+                    },
+                });
+
+                res.data.map((item) =>
+                    setUserStats((pre) => [...pre, { name: MONTHS[item._id - 1], 'New User': item.soluong }]),
+                );
+            } catch (error) {
+                console.log(error);
+            }
+        };
+
+        getStats();
+    }, []);
+    console.log(userStats);
+    return (
+        <Router>
+            <Topbar />
+            <div className="container">
+                <Sidebar />
+                <Switch>
+                    <Route exact path="/">
+                        <Home />
+                    </Route>
+                    <Route path="/users">
+                        <UserList />
+                    </Route>
+                    <Route path="/user/:userId">
+                        <User />
+                    </Route>
+                    <Route path="/newUser">
+                        <NewUser />
+                    </Route>
+                    <Route path="/products">
+                        <ProductList />
+                    </Route>
+                    <Route path="/product/:productId">
+                        <Product />
+                    </Route>
+                    <Route path="/newproduct">
+                        <NewProduct />
+                    </Route>
+                </Switch>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
